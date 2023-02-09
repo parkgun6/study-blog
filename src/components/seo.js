7f943950a414853de1f2a05a5998a49f65636a1c
parkgun6 <a01088807068@gmail.com>
2023-02-09 14:28:40 +0900

    gatsby initialize commit

---
+++ src/components/seo.js
@@ -0,0 +1,15 @@
+/**
+ * SEO component that queries for data with
+ * Gatsby's useStaticQuery React hook
+ *
+ * See: https://www.gatsbyjs.com/docs/how-to/querying-data/use-static-query/
+ */
+
+import * as React from "react"
+
+/* seo : 검색엔진 최적화 */
+const Seo = () => {
+  return <></>
+}
+
+export default Seo
